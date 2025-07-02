84e0c8d54b0d773a8de71284e3528cd71660cb24
Andre Rivera <42025878+andrej-rivera@users.noreply.github.com>
2025-07-01 22:15:31 -0700

    navbar complete
    completed a navbar, may need more margin editing later but looks alright for now

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,58 @@ function App() {
   return (
     <>
       {/* ==== front page ==== */}
-      <div className="column front-page">
-        <div className="front-name">MADISON SCHOOLEY</div>
-        <div className="front-title">ROCKET SCIENTIST</div>
+      <header className="column">
+        <h1>MADISON SCHOOLEY</h1>
+        <h2>ROCKET SCIENTIST</h2>
         <div className="row">
-          <button className="front-button front-button-resume"> RESUME</button>
+          <button className="button header-button-resume"> RESUME</button>
         </div>
 
         <div className="row">
-          <button className="front-button front-button-navbar"> About</button>
-          <button className="front-button front-button-navbar"> Projects</button>
-          <button className="front-button front-button-navbar"> Blog</button>
+          <button className="header-button-navbar"> About</button>
+          <button className="header-button-navbar"> Projects</button>
+          <button className="header-button-navbar"> Blog</button>
 
         </div>
 
-        <div  className="front-slogan"> IT'S NOT BRAIN SURGERY</div>
-      </div>
+        <h3> IT'S NOT BRAIN SURGERY</h3>
+      </header>
       
-      <div className="header"> hi i'm a header</div>
 
-      {/* ==== about me ==== */}
-      <div className="general">
+      {/* ==== everything else ==== */}
+      <main>
+        
+        {/* ==== navbar ==== */}
+        <nav className="row"> 
+            <h1> MADISON SCHOOLEY </h1>
+            <navbar>
+              <button className="navbar-button">About</button>
+              <button className="navbar-button">Projects</button>
+              <button className="navbar-button">Blog</button>
 
-      </div>
+            </navbar>
+
+            <button className="navbar-button-resume"> RESUME</button>
+        </nav>
+
+        {/* ==== about me ==== */}
+        <about className="row">
+          <div className="column">
+            <h1>Hi I'm Madz!</h1>
+            <p>
+              I am a graduate from San Jose State University with a Bachelor’s degree in aerospace engineering.  
+            </p>
+          </div>
+          <img src="/images/about-me.jpg" width="640" alt="Profile" className="about-me-image"/>
+        </about>
+
+        {/* ==== projects ==== */}
+        {/* ==== blog ==== */}
+        {/* ==== contact me ==== */}
+
+        
+
+      </main>
     </>
   )
 }
