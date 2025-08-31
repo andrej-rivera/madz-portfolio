f5de8306bec0b8edb3423500d02913c602b8cc79
Andre Rivera <42025878+andrej-rivera@users.noreply.github.com>
2025-08-31 01:05:32 -0700

    Update Resume Page for Whole Screen

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,13 +6,15 @@ export default function Resume() {
     PDFObject.embed("./resume.pdf", "#resume");
     return (
         <>
-            <main>
-                <Header/>
-                <div className="resume">
-                    <h1 className="title">Resume</h1>
-                    <iframe src="./resume.pdf" className="resume-container"> </iframe>
-                </div>
-            </main>
+            <div className="resume">
+                {/* <iframe src="./resume.pdf" className="resume-container"> </iframe> */}
+
+                <object data="./resume.pdf" type="application/pdf" width="100%" height="100%">
+                    <img width="100%" height="100%" src="images/resume.png"></img>
+                </object>
+            </div>
+
+
         </>
     )
 };
